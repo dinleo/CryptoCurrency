ac174d21d0a8c40608fb9b41cc195111fe45af2f
dinleo <dinleo11@gmail.com>
2022-01-14 18:01:23 +0900

    Explorer

--- main.go
+++ main.go
@@ -15,25 +15,39 @@ const (
 
 var tmpl *template.Template
 
-type homeData struct {
-	PageTitle string
-	Blocks    []*blockchain.Block
+type pageData struct {
+	PageTitle  string
+	PageHeader string
+	Blocks     []*blockchain.Block
 }
 
 func home(rw http.ResponseWriter, rq *http.Request) {
-	data := homeData{"Homepage", blockchain.GetBlockchain().AllBlocks()}
+	data := pageData{"HomePage", "HomePage", blockchain.GetBlockchain().AllBlocks()}
 	tmpl.ExecuteTemplate(rw, "home", data)
 }
 
-func add(rw http.ResponseWriter, rq *http.Request) {
-	tmpl.ExecuteTemplate(rw, "add", nil)
+func add(w http.ResponseWriter, r *http.Request) {
+	data := pageData{"AddPage", "Add Block", nil}
+	switch r.Method {
+	case "GET":
+		tmpl.ExecuteTemplate(w, "add", data)
+	case "POST":
+		r.ParseForm()
+		blockData := r.Form.Get("blockData")
+		blockchain.GetBlockchain().AddBlock(blockData)
+		http.Redirect(w, r, "/", http.StatusPermanentRedirect)
+		fmt.Println("Add Block", blockData)
+	}
 }
 
 func main() {
 	tmpl = template.Must(template.ParseGlob(templateDir + "pages/*.gohtml"))
 	tmpl = template.Must(tmpl.ParseGlob(templateDir + "partials/*.gohtml"))
+
 	http.HandleFunc("/", home)
 	http.HandleFunc("/add", add)
+
 	fmt.Printf("Listening on http://localhost%s\n", port)
 	log.Fatal(http.ListenAndServe(port, nil))
+
 }
